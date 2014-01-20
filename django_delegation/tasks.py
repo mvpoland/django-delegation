eacfdb01ef91a900a90a65298e67452be07490c1
Jef Geskens <jef.geskens@gmail.com>
2014-01-20 15:53:02 +0100

    Made Django Progress dependency optional

--- django_delegation/tasks.py
+++ django_delegation/tasks.py
@@ -1,12 +1,20 @@
 from celery.decorators import task
-from djprogress import with_progress, progress_error_reporter
-from django_delegation.utils import execute_delegated_view
+from .utils import execute_delegated_view
 from .models import DelegatedView
 
 
 @task
 def handle_delegated_view(dg_id):
     dg = DelegatedView.objects.get(pk=dg_id)
-    with progress_error_reporter():
-        for i in with_progress([None], name='Loading delegated view %s' % dg.url):
-            execute_delegated_view(dg)
+
+    # Try to encapsulate the task inside a Django Progress watcher.
+    # This way, we can take advantage of its exception reporter.
+    # If this does not work, we just proceed without it.
+    try:
+        from djprogress import with_progress, progress_error_reporter
+    except ImportError:
+        execute_delegated_view(dg)
+    else:
+        with progress_error_reporter():
+            for i in with_progress([None], name='Loading delegated view %s' % dg.url):
+                execute_delegated_view(dg)
